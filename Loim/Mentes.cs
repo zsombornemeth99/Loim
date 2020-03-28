94ef0bfa97114b85db5879a2f71f7a4dcf226c3a
Leviii99 <nyiro.levente@gmail.com>
2020-03-28 14:48:00 +0100

    Mentes kezdete

---
+++ Loim/Mentes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Loim
+{
+    class Mentes
+    {
+        private Kerdes k;
+
+        public Mentes(Kerdes k)
+        {
+            if (!File.Exists())
+        }
+
+    }
+}
