ceee8aa9ebda8b50903b3711bb99ef71c899f192
zsombornemeth99 <zsombor.nemeth99@gmail.com>
2020-03-29 17:37:24 +0200

    dicsőség oldal színezése

--- Loim/Jatek.cs
+++ Loim/Jatek.cs
@@ -348,9 +348,10 @@ namespace Loim
             Console.ReadKey();
 
             kerdes();
-            
+
             // ide kell a játék
             // ide jöhet a jatekInditastól különböző rész
+            ranglista();
             File.Delete("mentes.txt");
             Console.ReadKey();
         }
@@ -539,11 +540,47 @@ namespace Loim
                 rang.Add(sor);
             }
             r.Close();
-            rang.Sort();
+            string s = "TOP 3";
+            Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(s);
+            Console.ResetColor();
+            string[] adatok;
+            List<string> nev=new List<string>();
+            List<int> eredmeny= new List<int>();
+            List<long> ido=new List<long>();
             foreach (var item in rang)
             {
-                Console.WriteLine(item);
+                adatok = item.Split(';');
+                nev.Add(adatok[0]);
+                eredmeny.Add(int.Parse(adatok[1]));
+                ido.Add(long.Parse(adatok[2]));              
             }
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == 0) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n\t{0,-1}.hely\tNév: {1,-10}\tSzint: {2,-1}\tIdő: {3,-3}mp.", i + 1, nev[i], eredmeny[i], ido[i]);
+                    Console.ResetColor();
+                }
+                if (i==1)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\n\t{0,-1}.hely\tNév: {1,-10}\tSzint: {2,-1}\tIdő: {3,-3}mp.", i + 1, nev[i], eredmeny[i], ido[i]);
+                    Console.ResetColor();
+                }
+                if (i==2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\n\t{0,-1}.hely\tNév: {1,-10}\tSzint: {2,-1}\tIdő: {3,-3}mp.", i + 1, nev[i], eredmeny[i], ido[i]);
+                    Console.ResetColor();
+                }
+                
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n\n\tNyomjon egy ENTER-t a visszalépéshez!");
+            Console.ResetColor();
             Console.ReadKey();
         }
 
@@ -808,6 +845,5 @@ namespace Loim
             Console.ReadKey();
         }
 
-        
     }
 }
